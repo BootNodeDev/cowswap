8b12cb962ac553d894a1ecf685d9cd0e4984ab92
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-08-27 17:38:10 -0300

    feat: add feature flag to enable or disable locale loading

--- apps/cowswap-frontend/src/lib/i18n.tsx
+++ apps/cowswap-frontend/src/lib/i18n.tsx
@@ -1,15 +1,33 @@
 import { ReactNode, useEffect } from 'react'
 
 import { DEFAULT_LOCALE, SupportedLocale } from '@cowprotocol/common-const'
+import { isLinguiInternationalizationEnabled } from '@cowprotocol/common-utils'
 
 import { i18n } from '@lingui/core'
 import { I18nProvider } from '@lingui/react'
+
+/**
+ * Loads and activates the default locale (en-ES) for the application.
+ *
+ * This sets up the i18n instance with an empty message catalog for the default locale,
+ * ensuring that translation keys are used as fallbacks when no translations are available.
+ */
+const loadDefaultLocale = (): void => {
+  i18n.load(DEFAULT_LOCALE, {})
+  i18n.activate(DEFAULT_LOCALE)
+}
 
 // TODO: Add proper return type annotation
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export async function dynamicActivate(locale: SupportedLocale) {
   try {
+    if (!isLinguiInternationalizationEnabled) {
+      loadDefaultLocale()
+      return
+    }
+
     const catalog = await import(`../locales/${locale}.po`)
+
     // Bundlers will either export it as default or as a named export named default.
     i18n.load(locale, catalog.messages || catalog.default.messages)
     i18n.activate(locale)
@@ -20,9 +38,9 @@ export async function dynamicActivate(locale: SupportedLocale) {
 }
 
 interface ProviderProps {
+  children: ReactNode
   locale: SupportedLocale
   onActivate?: (locale: SupportedLocale) => void
-  children: ReactNode
 }
 
 // TODO: Add proper return type annotation
@@ -41,8 +59,7 @@ export function Provider({ locale, onActivate, children }: ProviderProps) {
   // as [there are no "default" messages](https://github.com/lingui/js-lingui/issues/388#issuecomment-497779030).
   // See https://github.com/lingui/js-lingui/issues/1194#issuecomment-1068488619.
   if (i18n.locale === undefined && locale === DEFAULT_LOCALE) {
-    i18n.load(DEFAULT_LOCALE, {})
-    i18n.activate(DEFAULT_LOCALE)
+    loadDefaultLocale()
   }
 
   return <I18nProvider i18n={i18n}>{children}</I18nProvider>
